d9b6f9a65c6db78ff8f0f17f4963fc9023b29129
thehambeard <61787444+thehambeard@users.noreply.github.com>
2024-11-12 08:18:54 -0500

    Move Font stuff to own namespace and removed unused references

---
+++ FontMod/FontSwap/FontCollection.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+namespace FontMod.FontSwap;
+
+public class FontCollection : ICollection<FontDataModel>
+{
+    private readonly HashSet<FontDataModel> _fontDataModels = [];
+
+    public int Count => _fontDataModels.Count;
+
+    public bool IsReadOnly => false;
+
+    public void Add(FontDataModel item)
+    {
+        if (item == null)
+            throw new ArgumentNullException("Cannot add null item to FontCollection");
+
+        if (_fontDataModels.Contains(item))
+            throw new ArgumentException($"FontCollection already contains {item.Font.name}");
+
+        _fontDataModels.Add(item);
+    }
+
+    public void Add(Font item) => Add(FontDataModel.CreateFromFont(item));
+    public void AddFromFilePath(string fontPath) => Add(FontDataModel.CreateFromFontPath(fontPath));
+    public void AddFromFolderPath(string folderPath)
+    {
+        if (!Directory.Exists(folderPath))
+            throw new DirectoryNotFoundException($"Folder path not found: {folderPath}");
+
+        foreach (var font in Directory.GetFiles(folderPath))
+            AddFromFilePath(font);
+    }
+
+    public FontDataModel GetFontByName(string name)
+    {
+        var model = _fontDataModels.Where(x => x.Name == name).First();
+
+        if (model == null)
+            throw new ArgumentException($"Font name {name} not found in collection");
+
+        return model;
+    }
+
+    public void Clear() => _fontDataModels.Clear();
+
+    public bool Contains(FontDataModel item) => _fontDataModels.Contains(item);
+
+    public void CopyTo(FontDataModel[] array, int arrayIndex) => _fontDataModels.CopyTo(array, arrayIndex);
+
+    public IEnumerator<FontDataModel> GetEnumerator() => _fontDataModels.GetEnumerator();
+
+    public bool Remove(FontDataModel item) => _fontDataModels.Remove(item);
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
---
+++ FontMod/FontSwap/FontSwapperPatches.cs
@@ -0,0 +1,61 @@
+﻿using FontMod.Utility;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using TMPro;
+namespace FontMod.FontSwap;
+
+[HarmonyPatch]
+public static class TMPTestPach
+{
+    [HarmonyPatch(typeof(TextMeshProUGUI), nameof(TextMeshProUGUI.LoadFontAsset))]
+    [HarmonyPrefix]
+    static void TextPatch(TextMeshProUGUI __instance)
+    {
+        if (__instance.m_fontAsset != null)
+            __instance.m_fontAsset = FontMapper.Instance.GetFontMapped(__instance.m_fontAsset);
+    }
+
+    [HarmonyPatch(typeof(MaterialReferenceManager), nameof(MaterialReferenceManager.TryGetFontAsset))]
+    [HarmonyPostfix]
+    static void TryGetFontAsset(ref TMP_FontAsset fontAsset)
+    {
+        if (fontAsset != null)
+            fontAsset = FontMapper.Instance.GetFontMapped(fontAsset);
+    }
+
+    [HarmonyPatch(typeof(TMP_Text), nameof(TMP_Text.ValidateHtmlTag))]
+    [HarmonyTranspiler]
+    static IEnumerable<CodeInstruction> TagPatch(IEnumerable<CodeInstruction> instructions, ILGenerator iLGen)
+    {
+        var method = AccessTools.Method(typeof(MaterialReferenceManager), nameof(MaterialReferenceManager.AddFontAsset));
+
+        var assetIntructions = new CodeInstruction[]
+        {
+            new(OpCodes.Call, method)
+        };
+
+        var instructionIndex = instructions.FindCodes(assetIntructions);
+
+        if (instructionIndex >= 0)
+        {
+            var ldlocs = instructions.ElementAt(instructionIndex - 1);
+
+            var patchCodes = new CodeInstruction[]
+            {
+                new(OpCodes.Call, AccessTools.PropertyGetter(typeof(FontMapper), nameof(FontMapper.Instance))),
+                new(OpCodes.Ldloc_S, ldlocs.operand),
+                new(OpCodes.Callvirt, AccessTools.Method(typeof(FontMapper), nameof(FontMapper.GetFontMapped))),
+                new(OpCodes.Stloc_S, ldlocs.operand)
+            };
+
+            return instructions.InsertRange(instructionIndex, patchCodes, true);
+        }
+        else
+        {
+            Main.Logger.Error("TagPatch Transpile Failed.");
+            return instructions;
+        }
+    }
+}
